e412b06478b3a7207f70d1ddae2ef2b669cabc47
ayeshamou <rocketscience968@gmail.com>
2018-02-11 15:37:16 +0600

    init mou

---
+++ app/src/main/java/com/example/group11/jagoron/Register.java
@@ -0,0 +1,18 @@
+package com.example.group11.jagoron;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Register extends Activity {
+	
+	
+
+}
