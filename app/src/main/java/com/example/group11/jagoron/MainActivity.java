e6c6ebc532dd3edc90298d57397fe9531f8faba5
ayeshamou <rocketscience968@gmail.com>
2018-02-11 15:43:24 +0600

    main activity added

--- app/src/main/java/com/example/group11/jagoron/MainActivity.java
+++ app/src/main/java/com/example/group11/jagoron/MainActivity.java
@@ -21,7 +21,32 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity{
 
-	
+	TextView banglaTextView;
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		banglaTextView = (TextView) findViewById(R.id.textView1);
+		Typeface myCustomFont = Typeface.createFromAsset(getAssets(), "fonts/SutonnyOMJ.ttf");
+		banglaTextView.setTypeface(myCustomFont);
+
+	}
+
+
+
+    public void verify(View v){
+		Intent Main = new Intent(this, Verify.class);
+		startActivity(Main);
+
+		}
+    public void register(View v){
+		Intent i_register=new Intent(MainActivity.this,Register.class);
+		startActivity(i_register);
+		}
+
+
 
 
 
