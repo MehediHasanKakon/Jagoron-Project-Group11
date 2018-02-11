bce60327e11c9c6088d6cb94d974b02607d7be28
sarowarhossainrocky1 <sarowarhossainrocky1@gmail.com>
2018-02-11 16:29:19 +0600

    init

---
+++ app/src/main/java/com/example/group11/jagoron/Instructions.java
@@ -0,0 +1,13 @@
+package com.example.group11.jagoron;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.View;
+import android.app.Activity;
+import android.content.Intent;
+import android.widget.TextView;
+
+public class Instructions extends Activity {
+
+	
+}
---
+++ app/src/main/java/com/example/group11/jagoron/Verify.java
@@ -0,0 +1,20 @@
+package com.example.group11.jagoron;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.support.v4.app.NavUtils;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Build;
+
+public class Verify extends Activity {
+
+	
+}
